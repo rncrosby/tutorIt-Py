bba2081f835fe293b063c7ae412d3bfb805a1b07
Robert Crosby <robertcrosby@Roberts-MacBook-Pro.local>
2018-03-08 16:47:29 -0800

    a

--- server.py
+++ server.py
@@ -5,7 +5,9 @@ app = Flask(__name__)
 def hello():
 	return "<h1 style='color:black'><center>All Systems Operational</center></h1>"
 
+@app.route("/verifyEmail/<email>/<code>")
+def verifyEmail(email,code):
+	return "<h1 style='color:black'><center>" + email + ", " + code + "</center></h1>"
+
 if __name__ == "__main__":
-	app.run(host='0.0.0.0')
-
-
+	app.run(host='0.0.0.0', threaded=True)
