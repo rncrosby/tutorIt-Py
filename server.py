143f07b61dc96a3d87f49a7682b1d337a52e2746
Robert Crosby <robertcrosby@Roberts-MacBook-Pro.local>
2018-03-14 14:12:42 -0700

    a

--- server.py
+++ server.py
@@ -5,12 +5,20 @@ from email.MIMEText import MIMEText
 import time
 from apns import APNs, Frame, Payload
 from flask import Flask
+import braintree
 
 app = Flask(__name__)
 
-apns_enhanced = APNs(use_sandbox=False, cert_file='cert/pro/apns-pro.pem', enhanced=True)
-apns = APNs(use_sandbox=False, cert_file='cert/pro/apns-pro.pem', key_file='cert/pro/apns-pro-key-noenc.pem')
+gateway = braintree.BraintreeGateway(
+  braintree.Configuration(
+    environment=braintree.Environment.Production,
+    merchant_id='j3thkst7k9j6mkvc',
+    public_key='y77kkm6dvvjcswqr',
+    private_key='5d0998038deb27cebd643b1c11a4bdf7'
+  )
+)
 
+apns_enhanced = APNs(use_sandbox=False, cert_file='cert/pro/apns-pro.pem', enhanced=True)
 def response_listener(error_response):
 	print ("client get error-response: " + str(error_response))
 
@@ -44,6 +52,10 @@ def sendPush(message,token):
 	apns_enhanced.gateway_server.send_notification(token, payload, identifier=identifier)
 	apns_enhanced.gateway_server.register_response_listener(response_listener)
 	return "Success"
+
+@app.route("/getClientToken")
+def client_token():
+  return gateway.client_token.generate()
 
 
 if __name__ == "__main__":
